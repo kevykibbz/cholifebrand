858b04fc06772dc45dd07501a45ff1872ebbb7d1
Kevin Kibebe <56401333+kevykibbz@users.noreply.github.com>
2021-12-19 18:54:57 +0300

    Update user.php

--- php/user.php
+++ php/user.php
@@ -1,7 +1,7 @@
 <?php 
-     $user=[
+      $user=[
      	        'site_name'=>'Cholifebrand',
-                  'email_address'=>'*******@gmail.com',
+                  'email_address'=>'RealusGeorgeJr@gmail.com',
                   'email_address2'=>'example2@duncho.com',
      	        'description'=>'sample description',
      	        'key_words'=>'sample description',
@@ -10,10 +10,10 @@
      	        'location'=>'Eldoret,Kenya',
      	        'designer_link'=>'https://mjtech.co.ke',
                   'designer_name'=>'mjtech',
-                  'facebook_link'=>'www.facebook.com',
-                  'twitter_link'=>'www.twitter.com',
-                  'linkedin_link'=>'www.linkedin.com',
-                  'instagram_link'=>'www.instagram.com',
+                  'designer portfolio'=>'https://chillcash.co.ke/portfolio/',
+                  'facebook_link'=>'https://web.facebook.com/kevy.kibbz/',
+                  'twitter_link'=>'https://twitter.com/Kevin36285655',
+                  'instagram_link'=>'https://www.instagram.com/kevviey/',
                   'whatsapp_link'=>'https://wa.link/r9fxm4'
      		]
  ?>
